9b51077f83b6e48a74330a31decda430863c9920
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2024-03-04 12:53:29 -0500

    Create User Model

--- api/index.js
+++ api/index.js
@@ -1,15 +1,16 @@
 import express from "express";
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Connected to MongoDB");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
-
-
+  });
 
 const app = express();
 
