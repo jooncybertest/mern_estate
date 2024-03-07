bdbca47937d5cbbaf7e18247e0f9790b7fe60cd1
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2024-03-06 19:40:12 -0500

    add redux toolkit

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -32,7 +32,7 @@ export default function SignUp() {
       }
       setLoading(false);
       setError(null);
-      navigate('/sign-in');
+      navigate("/sign-in");
       console.log(data);
     } catch (error) {
       setLoading(false);
